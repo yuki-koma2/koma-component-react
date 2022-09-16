75d3626f172d713c90c54e68fc20635f13d09b64
yukikoma <yuki.k.lion@gmail.com>
2022-09-16 17:29:48 +0900

    rebuild project tsx のセットアップ

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,37 @@
 import jsx from 'rollup-plugin-jsx'
 import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import styles from "rollup-plugin-styles";
+// import acornJsx from 'acorn-jsx';
+import commonjs from '@rollup/plugin-commonjs';
 
 // 設定はここを参照。カスタマイズしていく
 // https://rollupjs.org/guide/en/#installation
 
 export default {
   input:'src/index.ts',
+  // output形式をいくつか試す
   output: [
     {
-      file: 'dist/main.js',
+      file: 'dist/cjs/main.js',
       format: 'cjs',
     },
+    {
+      file: 'dist/ems/main.esm.js',
+      format: 'esm',
+    },
   ],
-  dest: 'build/app.js',
-  entry: 'src/index.js',
+  // acornInjectPlugins: [acornJsx()],
   plugins: [
     jsx( {factory: 'React.createElement'} ),
     resolve({
       moduleDirectories: ['node_modules']
-    })
+    }),
+    typescript({
+        exclude: ["src/stories/*","**/__tests__", "**/*.test.ts"],
+        compilerOptions: { jsx: 'preserve' }
+      }),
+    styles(),
+    commonjs(),
   ],
-  sourceMap: true
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// とりあえず都度export。後で自動的に判別してbundlerに渡せるようにする。
+export { Button } from "./atoms/Button/Button";
+
 export const hoge = () => {
      const number = 1 + 1;
     console.log(number);
